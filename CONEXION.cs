884b7c926bb9ecaa8eea739089c83deb6b0fa3b2
brenxaguilar <167571400+brenxaguilar@users.noreply.github.com>
2024-06-05 21:23:34 -0600

    Add files via upload

---
+++ CONEXION.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace BASE_DE_DATOS_ALUMNOS_UNI_3
+{
+    internal class CONEXION
+    {
+        public static SqlConnection connectar ()
+        {
+            SqlConnection cn = new SqlConnection("Server=" +
+                "BRENDA\\SQLEXPRESS;Database=REGISTRO;Integrated Security=True;");
+        cn.Open();
+            return cn;
+
+    }
+}
+}
